cb5a02fe03b2057f79b90064dbdcbd9431b27749
praktikant10 <praktikant10@BL-PRAKTIKANT10>
2023-05-22 14:41:53 +0200

    123456

---
+++ DUTYFREE/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using DUTYFREE.Models.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DUTYFREE.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> _products = new List<Product> { new Product { Name = "Pepsi", Price = 42 }, new Product { Name = "Cola", Price = 36} };
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
