2e1acd048de6f45ad845a6cb2dae21768b7db9dd
praktikant11 <praktikant11@BL-PRAKTIKANT11>
2023-05-26 12:26:19 +0200

    kdyztak smazat

--- DUTYFREE/Controllers/HomeController.cs
+++ DUTYFREE/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using DUTYFREE.Models;
+using DUTYFREE.Models.Products;
+using DUTYFREE.ViewModels.Products;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,6 +8,10 @@ namespace DUTYFREE.Controllers
 {
     public class HomeController : Controller
     {
+        public static List<Product> _products = new List<Product> { new Product { Name = "Pepsi", Price = 30 , ProductID = 0, Quantity = 6, ImageUrl = "/Images/Pepsi_obrazek.png", Format = "pepsi-image"},
+                                                                     new Product { Name = "Cola", Price = 36, ProductID = 1, ImageUrl ="/Images/Cola_obrazek.png", Format = "cola-image" },
+                                                                     new Product { Name = "Tatranka", Price = 10, ProductID = 2, ImageUrl="/Images/Tatranka_obrazek.png", Format = "tatranka-image"},
+                                                                     new Product { Name = "Corny", Price = 12, ProductID = 3, ImageUrl = "/Images/Corny_obrazek.png", Format = "corny-image" }};
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,7 +21,7 @@ namespace DUTYFREE.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(new ProductViewModel() { Products= _products });
         }
 
         public IActionResult Privacy()
---
+++ DUTYFREE/ViewModels/Products/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DUTYFREE.Models.Products;
+
+namespace DUTYFREE.ViewModels.Products
+{
+    public class ProductViewModel
+    {
+        public IList<Product>? Products { get; set; }
+
+    }
+}
