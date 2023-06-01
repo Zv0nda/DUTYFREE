d271cd9dea99998e01b9ddd8513e00dafad6789f
praktikant10 <praktikant10@BL-PRAKTIKANT10>
2023-06-01 13:56:52 +0200

    a

---
+++ DUTYFREE.DB/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (256) NULL,
+    [Email]    NVARCHAR (256) NULL,
+    [ImageUrl] VARCHAR (1024) NULL,
+    [Role]     INT            CONSTRAINT [DF_Users_Role] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
